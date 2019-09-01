1cfe38a18779c02a9579f399096f0f2fc914b5d4
xingbiyanshu <xingbiyanshu@gmail.com>
2019-09-01 11:23:11 +0800

    add object

--- HelloJavaScript.js
+++ HelloJavaScript.js
@@ -2,11 +2,12 @@
  * 
  */
 
-function func1() {
-	alert("Hello");
+function func1(content) {
+	alert(content);
 }
 
 function func2() {
-	func1();
+	var person = {firstName:"Bill", lastName:"Gates", age:62, eyeColor:"blue"};
+	func1(person.firstName);
 }
 
